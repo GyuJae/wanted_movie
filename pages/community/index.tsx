8bbf30561b527802ee4283be98205c5bbca4471d
Gyujae <wjdrbwo1206@naver.com>
2022-06-11 22:50:13 +0900

    ISR community home

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,10 +1,43 @@
 import type { NextPage } from 'next'
+import { PostWithUserAndCount } from 'types/post'
+import client from '@libs/client'
 import dynamic from 'next/dynamic'
 
 const Community = dynamic(() => import('@components/Community'), { ssr: false })
 
-const CommunityPage: NextPage = () => {
-  return <Community />
+interface IProps {
+  posts?: PostWithUserAndCount[]
+}
+
+export async function getStaticProps() {
+  const posts = await client?.post.findMany({
+    include: {
+      user: {
+        select: {
+          avatar: true,
+          username: true,
+        },
+      },
+      _count: {
+        select: {
+          Like: true,
+          Comment: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+  return {
+    props: {
+      posts: JSON.parse(JSON.stringify(posts)),
+    },
+  }
+}
+
+const CommunityPage: NextPage<IProps> = ({ posts }) => {
+  return <Community posts={posts} />
 }
 
 export default CommunityPage
